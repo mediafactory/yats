1ec66a2f6d01d21116583632e1aaa8f3409ddbf5
Henner <hg@miadi.net>
2014-06-24 19:28:14 +0200

    = sort order of tags

--- sites/web/web/models.py
+++ sites/web/web/models.py
@@ -31,7 +31,7 @@ def getGibthubTags():
     cache_name = 'yats.%s.%s.tags.github' % (owner, repo)
     tags = cache.get(cache_name)
     if tags:
-        return set(reversed(sorted(tags)))
+        return tags
     
     # https://developer.github.com/v3/repos/#list-tags
     result = []
@@ -56,19 +56,20 @@ def getGibthubTags():
     tags = json.loads(content)
     
     for tag in tags:
-        result.append((tag['name'], tag['name'],))
-    
+        result.append(tag['name'])
+    result = list(reversed(sorted(result)))
+    result = [(value, value,) for value in result]
     cache.set(cache_name, result, 60 * 10)
-    return set(reversed(sorted(result)))
+    return result
 
 class test(tickets):
     component = models.ForeignKey(ticket_component)
-    version = models.CharField(max_length=255, choices=lazy(getGibthubTags, set)())
+    version = models.CharField(max_length=255, choices=lazy(getGibthubTags, list)())
     keywords = models.CharField(max_length=255, blank=True)
     reproduction = models.TextField(null=True)
     billing_needed = models.NullBooleanField(default=None)
     billing_reason = models.TextField(null=True, blank=True)
     billing_done = models.NullBooleanField(default=None)
     solution = models.TextField(null=True, blank=True)
-    fixed_in_version = models.CharField(max_length=255, choices=lazy(getGibthubTags, set)(), blank=True)
+    fixed_in_version = models.CharField(max_length=255, choices=lazy(getGibthubTags, list)(), blank=True)
     deadline = models.DateField(null=True, blank=True)
