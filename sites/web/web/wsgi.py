4a420714ca17475215cc4c9cba1c3d7d5ac44c85
Henner <hg@miadi.net>
2015-05-05 11:42:10 +0200

    = restart apache on file changes = pysrc

--- sites/web/web/wsgi.py
+++ sites/web/web/wsgi.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*- 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
+#os.environ.setdefault("PYTHONPATH", "/vagrant_modules/pysrc/")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+# http://blog.dscpl.com.au/2008/12/using-modwsgi-when-developing-django.html
+import monitor
+monitor.start(interval=1.0)
