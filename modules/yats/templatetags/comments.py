a2796f7cace02b181e7c766410491b6104e6cb2c
Henner <hg@miadi.net>
2015-05-05 13:24:35 +0200

    + more icons for different tasks

--- modules/yats/templatetags/comments.py
+++ modules/yats/templatetags/comments.py
@@ -2,6 +2,8 @@ from django.template import Library
 
 register = Library()
 
+# 0 = nothing, 1 = close, 2 = reopen, 3 = ref, 4 = ticket changed, 5 = file added, 6 = comment added, 7 = reassign
+
 def comment_color(value):
     result = {
       0: lambda : 'info',
@@ -16,3 +18,18 @@ def comment_color(value):
     return result
 
 register.filter('comment_color', comment_color)
+
+def history_icon(value):
+    result = {
+      0: lambda : '',
+      1: lambda : 'fa-check',
+      2: lambda : 'fa-undo',
+      3: lambda : 'fa-link',
+      4: lambda : 'fa-pencil-square-o',
+      5: lambda : 'fa-file',
+      6: lambda : 'fa-comment',
+      7: lambda : 'fa-user',
+    }[value]()
+    return result
+
+register.filter('history_icon', history_icon)
