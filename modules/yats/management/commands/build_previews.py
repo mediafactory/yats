f29af3031896046fe7cf6b5d6238dd55c0304f88
Henner <hg@mediafactory.de>
2018-08-06 18:16:01 +0200

    = exclude access and image

--- modules/yats/management/commands/build_previews.py
+++ modules/yats/management/commands/build_previews.py
@@ -15,7 +15,7 @@ class Command(BaseCommand):
         if not os.path.exists(dest):
             os.makedirs(dest)
 
-        files = tickets_files.objects.all()
+        files = tickets_files.objects.exclude(content_type__icontains='access').exclude(content_type__icontains='image')
         for file in files:
             src = '%s%s.dat' % (settings.FILE_UPLOAD_PATH, file.id)
             if not os.path.isfile('%s/%s.preview' % (dest, file.id)):
