f4c10a457fce76b9ded1c6bca4cb1742e0800f75
paulbellmann <paul.bellmann@yahoo.de>
2018-08-17 16:03:32 +0200

    -= base.html is a bad place for this, bc not on every page there is a datefield

--- modules/bootstrap_toolkit/static/bootstrap_toolkit/js/init_datepicker.js
+++ modules/bootstrap_toolkit/static/bootstrap_toolkit/js/init_datepicker.js
@@ -2,4 +2,11 @@
     $(document).on('focus.django-bootstrap-toolkit.data-api click.django-bootstrap-toolkit.data-api', 'input[data-bootstrap-widget=datepicker][data-provide!="datepicker"]', function (e) {
         $(e.target).datepicker("show");
     });
+    
+    $(function() {
+        $('input[data-bootstrap-widget=datepicker][data-provide!="datepicker"]').datepicker({
+            todayBtn: 'linked',
+            todayHighlight: true
+        });
+    })
 })(jQuery);
