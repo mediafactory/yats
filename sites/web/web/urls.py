140c03e28773a2fa9a3b555dc94c9fff24d8da4d
Henner <hg@miadi.net>
2018-08-21 18:59:11 +0200

    + DJRADICALE

--- sites/web/web/urls.py
+++ sites/web/web/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from simple_sso.sso_client.client import Client
+from djradicale.views import DjRadicaleView, WellKnownView
 
 admin.autodiscover()
 
@@ -13,4 +14,15 @@ urlpatterns = [
     url(r'^', include('yats.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^client/', include(test_client.get_urls())),
+
+    url(r'^' + settings.DJRADICALE_CONFIG['server']['base_prefix'].lstrip('/'),
+        include('djradicale.urls', namespace='djradicale')),
+
+    # .well-known external implementation
+    url(r'^\.well-known/(?P<type>(caldav|carddav))$',
+        WellKnownView.as_view(), name='djradicale_well-known'),
+
+    # .well-known internal (radicale) implementation
+    # url(r'^\.well-known/(?P<type>(caldav|carddav))$',
+    #     DjRadicaleView.as_view(), name='djradicale_well-known'),
 ]
