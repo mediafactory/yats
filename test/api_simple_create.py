b53c8f44360301a4e47ca97c7b007973f8b9222b
Henner <hg@mediafactory.de>
2018-08-21 18:09:08 +0200

    - sys func

--- test/api_simple_create.py
+++ test/api_simple_create.py
@@ -7,5 +7,4 @@ ticket = {
     'assigned': None,
 }
 rpc_srv = xmlrpclib.ServerProxy('http://admin:admin@192.168.33.11:8080/rpc/', allow_none=True, use_datetime=True)
-#print rpc_srv.system.listMethods()
 info = rpc_srv.ticket.createSimple(ticket, True)
