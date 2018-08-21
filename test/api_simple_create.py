b6280fefe62d60402475d8f6ac31655ef87f377f
Henner <hg@mediafactory.de>
2018-08-21 17:43:24 +0200

    + simple rpc add test

---
+++ test/api_simple_create.py
@@ -0,0 +1,11 @@
+import xmlrpclib
+
+ticket = {
+    'summary': '2. new Ticket via API simple',
+    'description': 'nice desc',
+    'priority': None,
+    'assigned': None,
+}
+rpc_srv = xmlrpclib.ServerProxy('http://admin:admin@192.168.33.11:8080/rpc/', allow_none=True, use_datetime=True)
+#print rpc_srv.system.listMethods()
+info = rpc_srv.ticket.createSimple(ticket, True)
