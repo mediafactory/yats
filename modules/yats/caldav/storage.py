1f3e94dfc434889cac3f283d0a125c2abcd1b883
Henner <hg@mediafactory.de>
2018-08-23 12:58:57 +0200

    = encoding

--- modules/yats/caldav/storage.py
+++ modules/yats/caldav/storage.py
@@ -1,19 +1,4 @@
-# Copyright (C) 2014 Okami, okami@fuzetsu.info
-
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 3
-# of the License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-
+# -*- coding: utf-8 -*-
 import json
 import os
 import logging
@@ -291,4 +276,4 @@ class Collection(ical.Collection):
             # priority
 
         cal.vtodo.add('x-radicale-name').value = '%s.ics' % str(item.uuid)
-        return cal.serialize()
+        return cal.serialize().decode('utf-8')
