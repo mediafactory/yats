5a18c4b155e63d25ca0963b4727596c4debed6cf
Henner <hg@mediafactory.de>
2018-09-03 16:38:53 +0200

    + fileupload test

--- test/api_simple_create.py
+++ test/api_simple_create.py
@@ -10,3 +10,18 @@ rpc_srv = xmlrpclib.ServerProxy('http://admin:admin@192.168.33.11:8080/rpc/', al
 info = rpc_srv.ticket.createSimple(ticket, True)
 
 print 'ticket created #%s' % info[0]
+
+
+import httplib2
+import base64
+h = httplib2.Http()
+headers = {
+   'User-Agent': 'miadi',
+   'Authorization': 'Basic ' + base64.b64encode('admin:admin'),
+   'content-type':'text/plain',
+}
+(resp, content) = h.request("http://192.168.33.11:8080/tickets/upload/%s/?filename=test.txt" % info[0],
+                            "PUT", body="This is text",
+                            headers=headers )
+print resp
+#print content
