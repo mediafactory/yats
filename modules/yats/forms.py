6054fc54a26e35facf5bb76818101047dfbcd9ce
Henner <hg@miadi.net>
2018-09-14 13:20:00 +0200

    = ignore hasComments in form

--- modules/yats/forms.py
+++ modules/yats/forms.py
@@ -141,7 +141,7 @@ class TicketsForm(forms.ModelForm):
 
     class Meta:
         model = mod_cls
-        exclude = ['c_date', 'c_user', 'u_date', 'u_user', 'd_date', 'd_user', 'active_record', 'closed', 'close_date', 'last_action_date', 'keep_it_simple', 'uuid', 'hasAttachments']
+        exclude = ['c_date', 'c_user', 'u_date', 'u_user', 'd_date', 'd_user', 'active_record', 'closed', 'close_date', 'last_action_date', 'keep_it_simple', 'uuid', 'hasAttachments', 'hasComments']
 
 
 def get_simple_priority():
