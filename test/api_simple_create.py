15cabf02f0f3c68c7ce0703be63294042b3d6db1
Henner <hg@miadi.net>
2019-03-03 12:02:53 +0100

    = PEP8 = port 80

--- test/api_simple_create.py
+++ test/api_simple_create.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+import httplib2
+import base64
 import xmlrpclib
 
 ticket = {
@@ -6,22 +9,19 @@ ticket = {
     'priority': None,
     'assigned': None,
 }
-rpc_srv = xmlrpclib.ServerProxy('http://admin:admin@192.168.33.11:8080/rpc/', allow_none=True, use_datetime=True)
+rpc_srv = xmlrpclib.ServerProxy('http://admin:admin@192.168.33.11/rpc/', allow_none=True, use_datetime=True)
 info = rpc_srv.ticket.createSimple(ticket, True)
 
 print 'ticket created #%s' % info[0]
 
 
-import httplib2
-import base64
 h = httplib2.Http()
 headers = {
    'User-Agent': 'miadi',
    'Authorization': 'Basic ' + base64.b64encode('admin:admin'),
-   'content-type':'text/plain',
+   'content-type': 'text/plain',
 }
-(resp, content) = h.request("http://192.168.33.11:8080/tickets/upload/%s/?filename=test.txt" % info[0],
+(resp, content) = h.request("http://192.168.33.11/tickets/upload/%s/?filename=test.txt" % info[0],
                             "PUT", body="This is text",
-                            headers=headers )
+                            headers=headers)
 print resp
-#print content
