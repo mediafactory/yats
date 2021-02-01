abd64abaaa397c6cc1a337dd57642f1e71d5df22
Henner <hg@mediafactory.de>
2021-02-01 20:10:12 +0100

    + tra viaroot / sudo

--- modules/yats/shortcuts.py
+++ modules/yats/shortcuts.py
@@ -922,13 +922,13 @@ def convertPDFtoImg(pdf, dest=None):
         img = PythonMagick.Image()
         img.density('100')
         img.read('%s[0]' % pdf)
-        path, ext = os.path.splitext(pdf)
-        img.write('PNG8:%s.png' % path)
+        path, ext = os.path.splitext(os.path.basename(pdf))
+        img.write('PNG8:/tmp/%s.png' % path)
 
         if dest:
-            os.rename('%s.png' % path, dest)
+            os.rename('/tmp/%s.png' % path, dest)
         else:
-            return '%s.png' % path
+            return '/tmp/%s.png' % path
 
     except Exception as e:
         print(e)
@@ -945,16 +945,16 @@ class yatsCalledProcessError(subprocess.CalledProcessError):
 
 def convertOfficeTpPDF(office, mimetype=None):
     if mimetype and 'html' in mimetype.lower():
-        command = '/usr/bin/libreoffice --headless --invisible --infilter=HTML --convert-to pdf --outdir /tmp %s' % (office)
+        command = 'sudo /usr/bin/libreoffice --headless --invisible --infilter=HTML --convert-to pdf --outdir /root/ %s' % (office)
 
     elif mimetype and 'text' in mimetype.lower():
-        command = '/usr/bin/libreoffice --headless --invisible --infilter=Text --convert-to pdf --outdir /tmp %s' % (office)
+        command = 'sudo /usr/bin/libreoffice --headless --invisible --infilter=Text --convert-to pdf --outdir /root %s' % (office)
 
     elif mimetype and 'json' in mimetype.lower():
-        command = '/usr/bin/libreoffice --headless --invisible --infilter=Text --convert-to pdf --outdir /tmp %s' % (office)
+        command = 'sudo /usr/bin/libreoffice --headless --invisible --infilter=Text --convert-to pdf --outdir /root %s' % (office)
 
     else:
-        command = '/usr/bin/libreoffice --headless --invisible --convert-to pdf --outdir /tmp %s' % (office)
+        command = 'sudo /usr/bin/libreoffice --headless --invisible --convert-to pdf --outdir /root %s' % (office)
     print(command)
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, cwd='.')
     output, unused_err = process.communicate()
@@ -964,4 +964,4 @@ def convertOfficeTpPDF(office, mimetype=None):
 
     path, fileName = os.path.split(office)
 
-    return '/tmp/%s.%s' % (fileName.split('.')[0], 'pdf')
+    return '/root/%s.%s' % (fileName.split('.')[0], 'pdf')
