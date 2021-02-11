c5feb6c5bf75166818f9776643c0dda100189555
Henner <hg@mediafactory.de>
2021-02-11 18:44:55 +0100

    = py3 + impersonate test

--- test/api_simple_create.py
+++ test/api_simple_create.py
@@ -1,7 +1,14 @@
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import httplib2
 import base64
 from xmlrpc import client as xmlrpclib
+
+class SpecialTransport(xmlrpclib.Transport):
+    def send_content(self, connection, request_body):
+        connection.putheader("Impersonate-User", "mister.mould")
+        super().send_content(connection, request_body)
+
 
 ticket = {
     'summary': '2. new Ticket via API simple',
@@ -9,7 +16,8 @@ ticket = {
     'priority': None,
     'assigned': None,
 }
-rpc_srv = xmlrpclib.ServerProxy('http://admin:admin@192.168.33.11/rpc/', allow_none=True, use_datetime=True)
+
+rpc_srv = xmlrpclib.ServerProxy('http://admin:admin@192.168.33.11/rpc/', allow_none=True, use_datetime=True, transport=SpecialTransport())
 info = rpc_srv.ticket.createSimple(ticket, True)
 
 print('ticket created #%s' % info[0])
@@ -18,7 +26,8 @@ h = httplib2.Http()
 headers = {
    'User-Agent': 'miadi',
    'Authorization': 'Basic YWRtaW46YWRtaW4=',
-   'content-type': 'text/plain',
+   'Content-Type': 'text/plain',
+   'Impersonate-User': 'mister.mould',
 }
 (resp, content) = h.request("http://192.168.33.11/tickets/upload/%s/?filename=test.txt" % info[0],
                             "PUT", body="This is text",
