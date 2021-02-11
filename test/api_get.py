5e2e44ef677bfe587c6f2b972dba96ded4764980
Henner <hg@mediafactory.de>
2021-02-11 18:44:31 +0100

    = py3

--- test/api_get.py
+++ test/api_get.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import base64
 from xmlrpc import client as xmlrpclib
--- test/api_upload_file.py
+++ test/api_upload_file.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 from xmlrpc import client as xmlrpclib
 
